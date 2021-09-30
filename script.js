37697e6340688a7243597cd946f09bb3c95bc9de
Ismael Perdomo <isma.perdomo.2@gmail.com>
2021-09-30 09:47:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const stock = []
-const uno = {name:"tornillos", description:"uno", stock:"350", precio:"5", minStock:"200"};
-const dos = {name:"clavos", description:"dos", stock:"600", precio:"4", minStock:"60"};
-const tres = {name:"ladrillos", description:"tres", stock:"160", precio:"30", minStock:"100"};
-const cuatro = {name:"tablas", description:"cuatro", stock:"26", precio:"20", minStock:"30"};
-const cinco = {name:"tornillos2", description:"cinco", stock:"250", precio:"5", minStock:"140"};
-const seis = {name:"clavos2", description:"seis", stock:"500", precio:"4", minStock:"75"};
-const siete = {name:"ladrillos2", description:"siete", stock:"80", precio:"30", minStock:"100"};
-const ocho = {name:"tablas2", description:"nueve", stock:"1700", precio:"25", minStock:"30"};
-const nueve = {name:"tablas3", description:"ocho", stock:"14", precio:"25", minStock:"30"};
-const diez = {name:"tablas4", description:"diez", stock:"85", precio:"25", minStock:"30"};
+const uno = {name:"tornillos", description:"uno", stock:"350", cost:"5", minStock:"200"};
+const dos = {name:"clavos", description:"dos", stock:"600", cost:"4", minStock:"60"};
+const tres = {name:"ladrillos", description:"tres", stock:"160", cost:"30", minStock:"100"};
+const cuatro = {name:"tablas", description:"cuatro", stock:"26", cost:"20", minStock:"30"};
+const cinco = {name:"tornillos2", description:"cinco", stock:"250", cost:"5", minStock:"140"};
+const seis = {name:"clavos2", description:"seis", stock:"500", cost:"4", minStock:"75"};
+const siete = {name:"ladrillos2", description:"siete", stock:"80", cost:"30", minStock:"100"};
+const ocho = {name:"tablas2", description:"nueve", stock:"1700", cost:"25", minStock:"30"};
+const nueve = {name:"tablas3", description:"ocho", stock:"14", cost:"25", minStock:"30"};
+const diez = {name:"tablas4", description:"diez", stock:"85", cost:"25", minStock:"30"};
 
 function add(){
     stock.push(uno)
@@ -22,3 +22,5 @@ function add(){
     stock.push(nueve)
     stock.push(diez)
 }
+
+add()
