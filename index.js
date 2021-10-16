20bb3ede568fb23c1957a0533c077d7962a0a06c
Axel Lois <axel9034@hotmail.com>
2021-10-15 21:02:28 -0300

    Funcion de borrar lista

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ let tbody = document.getElementById("tbody");
 let editButton = document.getElementsByClassName("editar");
 let deleteButton = document.getElementsByClassName("eliminar");
 let row = document.getElementsByClassName("filas");
-
+let arreglo = JSON.parse(localStorage.getItem("Datos"));
 
 
 function mostrarProductos() { //Muestra los productos.
-    let array = JSON.parse(localStorage.getItem("Datos"));
-    for (let i = 0; i < array.length; i++) {
+    tbody.innerHTML = ""; //Limpio la tabla por si debo volver a usar la funcion
+    for (let i = 0; i < arreglo.length; i++) {
         tbody.innerHTML += `<tr class="filas">    
-        <td>${array[i].name}</td>
-        <td>${array[i].description}</td>
-        <td>${array[i].stock}</td>
-        <td class="text-right"> $${array[i].unitCost}</td>
-        <td class="identificadores">${array[i].ID}</td>
+        <td>${arreglo[i].name}</td>
+        <td>${arreglo[i].description}</td>
+        <td>${arreglo[i].stock}</td>
+        <td class="text-right"> $${arreglo[i].unitCost}</td>
+        <td class="identificadores">${arreglo[i].ID}</td>
         <td class="text-center">
-            <button type="button" class="btn btn-success editar" id="editar">Editar</button>
-            <button type="button" onclick="borrarProducto(${array[i].ID},${array})" class="btn btn-danger eliminar" id="eliminar">Eliminar</button>
+            <button type="button" onclick="editarProducto(${i})" class="btn btn-success editar" id="editar">Editar</button>
+            <button type="button" onclick="borrarProducto(${arreglo[i].ID},${i})" class="btn btn-danger eliminar" id="eliminar">Eliminar</button>
         </td>
     </tr>`
         
@@ -28,18 +28,19 @@ function mostrarProductos() { //Muestra los productos.
         //     row[i].classList.add("table-danger");
         // }
     };
-    borrarProducto();
 };
 
 
-function borrarProducto(ID,array) {
-    if(compararID(ID)) {
-        
-
-    }
+function borrarProducto(ID,index) { //Saca el producto del localStorage.w
+    arreglo.splice(index,1);
+    localStorage.setItem("Datos",JSON.stringify(arreglo));
+    mostrarProductos(); //Debo volver a mostrar todo pero sin ese elemento
 };
 
-function editarProducto() {
+function editarProducto(index) {
+    
+
+
 
 };
 
